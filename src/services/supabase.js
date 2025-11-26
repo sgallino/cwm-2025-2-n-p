adcc77a86056b720a1496f1cdc5d6009625615d0
Santiago Gallino <gallino.santi@gmail.com>
2025-11-26 19:50:30 -0300

    Mueve las credenciales de Supabase a para usarse con [.env.local]

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -3,7 +3,7 @@
 // sus funciones o métodos.
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://jmfwpnpllqlwklofpois.supabase.co";
-const SUPABASE_KEY = "sb_publishable_nolHIB26HShPefDzFRrunA_2LDFycDc";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
